d80cb005d14c77509eb43872c0642072515146de
ricfrank <riccardo.franconi@gmail.com>
2012-09-01 17:16:52 +0200

    altri test funzionali per il controllore del blog

--- src/Blogger/BlogBundle/Tests/Controller/BlogControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/BlogControllerTest.php
@@ -13,4 +13,14 @@ class BlogControllerTest extends WebTestCase
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("titolo: ciccio-mario")')->count());
     }
+    
+    public function testUpdated()
+    {
+        $client = static::createClient();
+//        $client->insulate(); //isolamento del client - esclude ogni altra richiesta in quel processo per evitare side effets
+
+        $client->request('GET', '/blog/modifica-post/titolo:-fonzi-cicic');
+        
+        $this->assertTrue($client->getResponse()->isRedirect('/blog'));
+    }
 }
